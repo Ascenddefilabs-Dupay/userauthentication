d68fa69e52789148362e6a39bdc360627add1e0d
shivaprasadGow <Shivaprasad.G@ascenddefi.com>
2024-09-04 12:23:52 +0530

    google-signup

--- backend-userauthentication/signin/serializers.py
+++ backend-userauthentication/signin/serializers.py
@@ -6,6 +6,10 @@ from django.contrib.auth.hashers import check_password
 from django.contrib.auth import authenticate
 import logging
 
+# class ProjectSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Project
+#         fields = '__all__'
 
 
 
