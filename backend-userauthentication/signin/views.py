6355265703917c25a88373a5956b87fee14452b5
Saimamidala999 <Sai.Mamidala@ascenddefi.com>
2024-10-30 18:27:56 +0530

    i have completed google login backend

--- backend-userauthentication/signin/views.py
+++ backend-userauthentication/signin/views.py
@@ -22,7 +22,6 @@ import jwt
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .models import FiatWallet
-from .serializers import CustomUserSerializer
 
 SECRET_KEY = 'YOUR_SECRET_KEY'
 
